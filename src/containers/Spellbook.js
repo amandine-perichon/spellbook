919c52391e1ee293a17723a30573079f037acf64
Amandine Perichon <apericho@gmail.com>
2016-11-07 22:06:49 +1300

    Add redux-persist

--- src/containers/Spellbook.js
+++ src/containers/Spellbook.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {createStore, applyMiddleware, compose } from 'redux'
-import {Provider} from 'react-redux'
+import { persistStore, autoRehydrate } from 'redux-persist'
+import { Provider } from 'react-redux'
+import { AsyncStorage } from 'react-native'
 
 import HomeContainer from '../containers/HomeContainer'
 import reducers from '../reducers'
 
-let store = createStore(reducers, compose(
-    global.reduxNativeDevTools ?
-      global.reduxNativeDevTools(/*options*/) :
-      noop => noop
-  ))
+const enhancers = compose(
+  autoRehydrate(),
+  global.reduxNativeDevTools ?
+        global.reduxNativeDevTools() :
+        x => x
+)
+
+const store = createStore(reducers, undefined, enhancers)
+const persistor = persistStore(store, {storage: AsyncStorage})
 
 export default React.createClass({
   render() {
     return (
-      <Provider store={store}>
+      <Provider store={store} persistor={persistor}>
         <HomeContainer />
       </Provider>
     )
